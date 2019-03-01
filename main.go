2272d41c6d3b36af18f0d91cedefd1f9d8cb5e71
Ernesto <ernesto.gmz.zcrs@gmail.com>
2019-03-01 16:53:48 -0600

    manipulando str

--- main.go
+++ main.go
@@ -6,31 +6,14 @@ const helloWorld string = "hola %s %s, bienvenidos al facinante mundo de go "
 const testConst = "Test"
 
 func main(){
+    fmt.Println("Cadena con utf 8")
+    stringutf8 := getUnicode()
+    fmt.Println(stringutf8)
+    fmt.Println("hola"[2])
+    fmt.Println("cantidadlen de letras que tiene el caracter hola",len("hola ke tal"))
     
-    w, k := getDatosUsuario()
-    suma := suma(w, k)
-    resta := resta(w, k)
-    fmt.Println("El resultado de la suma de los numeros es: ")
-    fmt.Printf("%d", suma)
-    fmt.Println("El resultado de la resta de los numeros es: ")
-    fmt.Printf("%d", resta)
  }
 
- func getDatosUsuario() (int, int) {
-     var r, g int
-     fmt.Print("Ingresa el primer numero")
-     fmt.Scanf("%d", &r)
-     fmt.Print("Ingresa el segundo numero")
-     fmt.Scanf("%d", &g)
-     return r, g
- }
-
-func suma(a int, b int) int {
-  return a + b
+func getUnicode() string {
+    return "もしもし!"
 }
-
-func resta(c int, d int ) int {
-    return c - d
-}
-
-
