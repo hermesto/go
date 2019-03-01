e8006c05322d38748cf68ec4ea9e3e7758975911
Ernesto <ernesto.gmz.zcrs@gmail.com>
2019-03-01 16:34:42 -0600

    calculadora chaca

--- main.go
+++ main.go
@@ -6,32 +6,31 @@ const helloWorld string = "hola %s %s, bienvenidos al facinante mundo de go "
 const testConst = "Test"
 
 func main(){
-	name := getName()
-	number := suma(50, 50)
-  a, b, c := getVariables()
-  lastname := " Oh si malnacidos!!! "
-  name = "variable creada modificada en el acto"
-  fmt.Print("Ingresa tu nombre: ")
-  fmt.Scanf("%s", &name)
-  fmt.Printf( helloWorld, name, lastname)
-  fmt.Println(number, a, b, c)
+    
+    w, k := getDatosUsuario()
+    suma := suma(w, k)
+    resta := resta(w, k)
+    fmt.Println("El resultado de la suma de los numeros es: ")
+    fmt.Printf("%d", suma)
+    fmt.Println("El resultado de la resta de los numeros es: ")
+    fmt.Printf("%d", resta)
  }
 
-func getName() string{
-  var name string
-  name = "sin nombre"
-  fmt.Print("Ingresa tu nombre: ")
-  fmt.Scan("%s", &name)
-  return name
-}
-func getVariables() ( int, int, int){
-  return 3, 5, 7
-}
+ func getDatosUsuario() (int, int) {
+     var r, g int
+     fmt.Print("Ingresa el primer numero")
+     fmt.Scanf("%d", &r)
+     fmt.Print("Ingresa el segundo numero")
+     fmt.Scanf("%d", &g)
+     return r, g
+ }
 
 func suma(a int, b int) int {
   return a + b
 }
 
-
+func resta(c int, d int ) int {
+    return c - d
+}
 
 
