6f42457ef9fc44dfdba4c713dc04070a9af98304
Ernesto <ernesto.gmz.zcrs@gmail.com>
2019-03-01 17:04:25 -0600

    array y slice

--- main.go
+++ main.go
@@ -11,9 +11,25 @@ func main(){
     fmt.Println(stringutf8)
     fmt.Println("hola"[2])
     fmt.Println("cantidadlen de letras que tiene el caracter hola",len("hola ke tal"))
-    
+    getArray()
+    getSlice()
  }
 
 func getUnicode() string {
     return "もしもし!"
+}
+
+func getArray() {
+    var arr1 [2]string
+    arr2 := [3]int{1, 2, 3}
+    arr1[0] = "array"
+    arr1[1] = "array 2"
+    fmt.Println(arr1)
+    fmt.Println(arr2)
+}
+
+func getSlice(){
+    var slice1 []string
+    slice1 = append(slice1, "mi", "slice", "uno")
+    fmt.Println(slice1)
 }
